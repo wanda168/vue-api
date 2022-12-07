4c198facf735e845c60fe3b76e71f4d4564bce55
wanda <vdking.168@gmail.com>
2022-12-07 21:23:15 +0700

    add comment

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,16 +8,21 @@ import (
 	"os"
 )
 
+// config is the type for all application configuration
 type config struct {
-	port int
+	port int // what port do we want the web server to listen on
 }
 
+// application is the type for all data we want to share with
+// various parts of our application. We will share this information in most
+// cases by using this type as the receiver for functions
 type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
 }
 
+// main is the main entry point for our application
 func main() {
 	var cfg config
 	cfg.port = 8081
@@ -38,6 +43,7 @@ func main() {
 
 }
 
+// serve starts the web server
 func (app *application) serve() error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var payload struct {
