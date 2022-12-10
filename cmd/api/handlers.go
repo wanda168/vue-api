29633db0b34de4d684201c6f959541d1dbcab8cd
wanda <vdking.168@gmail.com>
2022-12-10 23:33:51 +0700

    improve jsonresponse type with an evelope, update login handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"time"
 )
 
 // jsonResponse is the type used for generic JSON responses
 type jsonResponse struct {
 	Error   bool        `json:"error"`
 	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+	Data    interface{} `json:"data,omitemply"`
 }
+
+type envelope map[string]interface{}
 
 // Login is the handler used to attempt to log a user into the api
 func (app *application) Login(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +36,40 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	// TODO: authenticate
 	app.infoLog.Println(creds.UserName, creds.Password)
 
+	// look up the user by email
+	user, err := app.models.User.GetByEmail(creds.UserName)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+
+	// validate the user's password
+	validPassword, err := user.PasswordMatches(creds.Password)
+	if err != nil || !validPassword {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+
+	// we have a valid user, so generate a token
+	token, err := app.models.Token.GenerateToken(user.ID, 24*time.Hour)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// save it to the database
+	err = app.models.Token.Insert(*token, *user)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
 	// send back a response
-	payload.Error = false
-	payload.Message = "Signed in"
+	payload = jsonResponse{
+		Error:   false,
+		Message: "logged in",
+		Data:    envelope{"token": token},
+	}
 
 	err = app.writeJSON(w, http.StatusOK, payload)
 	if err != nil {
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"strings"
 )
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
@@ -47,6 +48,8 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	return nil
 }
 
+// errorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
 func (app *application) errorJSON(w http.ResponseWriter, err error, status ...int) {
 	statusCode := http.StatusBadRequest
 
@@ -54,9 +57,25 @@ func (app *application) errorJSON(w http.ResponseWriter, err error, status ...in
 		statusCode = status[0]
 	}
 
+	var customErr error
+
+	switch {
+	case strings.Contains(err.Error(), "SQLSTATE 23505"):
+		customErr = errors.New("duplicate value violates unique constraint")
+		statusCode = http.StatusForbidden
+	case strings.Contains(err.Error(), "SQLSTATE 22001"):
+		customErr = errors.New("the value you are trying to insert is too large")
+		statusCode = http.StatusForbidden
+	case strings.Contains(err.Error(), "SQLSTATE 23503"):
+		customErr = errors.New("foreign key violation")
+		statusCode = http.StatusForbidden
+	default:
+		customErr = err
+	}
+
 	var payload jsonResponse
 	payload.Error = true
-	payload.Message = err.Error()
+	payload.Message = customErr.Error()
 
 	app.writeJSON(w, statusCode, payload)
 }
