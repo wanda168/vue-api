d226509cc8872b489686358af449d379dd71b050
wanda <vdking.168@gmail.com>
2022-12-10 21:12:09 +0700

    trying out the Token type's Insert function

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -66,11 +66,44 @@ func (app *application) routes() http.Handler {
 		token, err := app.models.User.Token.GenerateToken(2, 60*time.Minute)
 		if err != nil {
 			app.errorLog.Println(err)
+			return
 		}
 
 		token.Email = "admin@example.com"
 		token.CreatedAt = time.Now()
 		token.UpdatedAt = time.Now()
+
+		payload := jsonResponse{
+			Error:   false,
+			Message: "success",
+			Data:    token,
+		}
+
+		app.writeJSON(w, http.StatusOK, payload)
+	})
+
+	mux.Get("/test-save-token", func(w http.ResponseWriter, r *http.Request) {
+		token, err := app.models.User.Token.GenerateToken(2, 60*time.Minute)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		user, err := app.models.User.GetOne(2)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		token.UserID = user.ID
+		token.CreatedAt = time.Now()
+		token.UpdatedAt = time.Now()
+
+		err = token.Insert(*token, *user)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
 
 		payload := jsonResponse{
 			Error:   false,
