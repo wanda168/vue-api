bbdae27cd8c0cdabd4eb2ba0c1dc4498bff6fcee
wanda <vdking.168@gmail.com>
2022-12-11 16:08:36 +0700

    an aside: speeding things up when writing json in production:

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,10 +18,11 @@ type config struct {
 // various parts of our application. We will share this information in most
 // cases by using this type as the receiver for functions
 type application struct {
-	config   config
-	infoLog  *log.Logger
-	errorLog *log.Logger
-	models   data.Models
+	config      config
+	infoLog     *log.Logger
+	errorLog    *log.Logger
+	models      data.Models
+	environment string
 }
 
 // main is the main entry point for our application
@@ -33,6 +34,7 @@ func main() {
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	dsn := os.Getenv("DSN")
+	environment := os.Getenv("ENV")
 	db, err := driver.ConnectPostgres(dsn)
 	if err != nil {
 		log.Fatal("Cannot connect to database")
@@ -40,10 +42,11 @@ func main() {
 	defer db.SQL.Close()
 
 	app := &application{
-		config:   cfg,
-		infoLog:  infoLog,
-		errorLog: errorLog,
-		models:   data.New(db.SQL),
+		config:      cfg,
+		infoLog:     infoLog,
+		errorLog:    errorLog,
+		models:      data.New(db.SQL),
+		environment: environment,
 	}
 
 	err = app.serve()
